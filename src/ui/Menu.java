dfd1a07f4a7209ab98889864462e782ad8a8b1a0
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2019-10-05 00:50:50 -0500

    Search method added, minor changes in interface made

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,6 +1,6 @@
 package ui;
 
-import package.model.*;
+import model.*;
 import java.util.Scanner;
 
 public class Menu
@@ -34,12 +34,12 @@ public class Menu
 
 		while (on == true)
 		{
-			System.out.println("\n+-------------------------------------------------------------------------------------------+");
-			System.out.println("                       ~*| Welcome to Heaven's Door: Archangel edition |*~\n                     ");
-			System.out.println("Enter the number of the function you want to do.");
+			System.out.println("\n+---------------------------------------------~---------------------------------------------+");
+			System.out.println("                          ~*| Heaven's Door : Archangel edition |*~                          ");
+			System.out.println("\nEnter the number of the function you want to do.");
 			System.out.println("There are currently " + angelNumber + " archangels created.");
-			System.out.println("\n1 = Create a new archangel.\n2 = Search an archangel.\n3 = Display celebration dates.\n4 = Exit.");
-			System.out.println("+-------------------------------------------------------------------------------------------+");
+			System.out.println("\n1 = Create a new archangel.\n2 = Display details of an archangel.\n3 = Display celebration dates.\n4 = Exit.");
+			System.out.println("+---------------------------------------------~---------------------------------------------+");
 
 			int decision = reader.nextInt();
 
@@ -72,6 +72,7 @@ public class Menu
 		boolean good; 
 
 		System.out.println("                                <<<Archangel creation menu>>>                                ");
+		
 		System.out.println("\nEnter the name of the archangel (No point at the end).");
 		String name = reader.next();
 		
@@ -82,9 +83,10 @@ public class Menu
 			
 			System.out.println("\nEnter the photo's url of the archangel.");
 			String photo = reader.next();
-			
+			reader.nextLine();
+
 			System.out.println("\nEnter the prayer of the archangel.");
-			String prayer = reader.next();
+			String prayer = reader.nextLine();
 			
 			System.out.println("\nEnter the power of the archangel (Only one, no point at the end. ex: Healing).");
 			String power = reader.next();
@@ -113,15 +115,17 @@ public class Menu
 						
 						System.out.println("\nEnter the size of the candle. (1=small, 2=medium, 3=large).");
 						int size = reader.nextInt();
+						reader.nextLine();
 
 						good = maximumSuperior.sizeValidator(size);
 
 						if (good == true)
 						{
 							System.out.println("\nEnter the essence of the candle.");
-							String essence = reader.next();
+							String essence = reader.nextLine();
 
 							System.out.println("\nEnter the illuminance degree of the candle.");
+							
 							double illuminanceDegree = reader.nextDouble();
 
 							maximumSuperior.addArchangel(name, photo, prayer, power, month, day, color, size, essence, illuminanceDegree);
@@ -140,6 +144,11 @@ public class Menu
 
 	public void searchMenu ()
 	{
+		System.out.println("                                 <<<Archangel detail menu>>>                                 ");
+		
+		System.out.println("\nEnter the name or the power of the archangel.");
+		String key = reader.next();
+		maximumSuperior.displayArchangelInfo(key);
 
 	}
 
@@ -151,7 +160,7 @@ public class Menu
 	public void init ()
 	{
 		maximumSuperior.addArchangel ("Gabriel", "https://www.archangels.com/photos/gabriel.jpeg", "Glorioso Arcangel Gabriel, fortaleza de Dios, principe entre los espiritus angelicos, representante del Altisimo, que fuiste escogido para anunciar a la Santisima Virgen la Encarnacion: yo te suplico que ruegues a Dios por mi, para logre gozar el fruto de la redencion divina en la gloria del Cielo. Amen.", "Comunication", 3, 18, "Yellow", 2, "Vanilla", 10.5);
-		maximumSuperior.addArchangel ("Miguel", "https://www.archangels.com/photos/miguel.jpeg", "San Miguel Arcangel, defiendenos en la lucha. Sé nuestro amparo contra la perversidad y acechanzas del demonio. Que Dios manifieste sobre el su poder, es nuestra humilde suplica. Y tu, oh Principe de la Milicia Celestial, con el poder que Dios te ha conferido, arroja al infierno a Satanas, y a los demas espiritus malignos que vagan por el mundo para la perdición de las almas. Amen.", "Courage", 9, 29, "Red", 3, "Cinnamon", 12);
-		maximumSuperior.addArchangel ("Rafael", "https://www.archangels.com/photos/rafel.jpeg", "Oh poderoso Principe de la gloria San Rafael, llamado medicina de Dios, salud de los enfermos, luz de los ciegos, guia de caminantes, protector de la limosna, del ayuno y de la oracion: por aquella caridad con que acompaniaste al joven Tobias, te pido, oh glorioso protector mio, me libres de todos los males y peligros, y me acompanies en la peregrinacion de esta vida mortal, para llegar felizmente a puerto de salvacion en la eterna. Amen.", "Protection", 10, 24, "White", 1, "Mint", 7.75);
+		maximumSuperior.addArchangel ("Miguel", "https://www.archangels.com/photos/miguel.jpeg", "San Miguel Arcangel, defiendenos en la lucha. Se nuestro amparo contra la perversidad y acechanzas del demonio. Que Dios manifieste sobre el su poder, es nuestra humilde suplica. Y tu, oh Principe de la Milicia Celestial, con el poder que Dios te ha conferido, arroja al infierno a Satanas, y a los demas espiritus malignos que vagan por el mundo para la perdicion de las almas. Amen.", "Courage", 9, 29, "Red", 3, "Cinnamon", 12);
+		maximumSuperior.addArchangel ("Rafael", "https://www.archangels.com/photos/rafael.jpeg", "Oh poderoso Principe de la gloria San Rafael, llamado medicina de Dios, salud de los enfermos, luz de los ciegos, guia de caminantes, protector de la limosna, del ayuno y de la oracion: por aquella caridad con que acompaniaste al joven Tobias, te pido, oh glorioso protector mio, me libres de todos los males y peligros, y me acompanies en la peregrinacion de esta vida mortal, para llegar felizmente a puerto de salvacion en la eterna. Amen.", "Protection", 10, 24, "White", 1, "Mint", 7.75);
 	}
 }
